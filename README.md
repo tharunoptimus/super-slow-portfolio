70490c0cfdd035c62d17f81938799d49bfca08da
tharunoptimus <tharunoptimus@outlook.com>
2023-03-05 02:41:11 +0530

    Added README to remind about env variables

---
+++ README.md
@@ -0,0 +1,11 @@
+# Portfolio
+
+## Include these Environment Variables in your .env file before deploying
+
+```bash
+VITE_APP_EMAILJS_SERVICE_ID
+VITE_APP_EMAILJS_TEMPLATE_ID
+VITE_APP_EMAILJS_PUBLIC_KEY
+VITE_APP_EMAILJS_TO_NAME
+VITE_APP_EMAILJS_TO_EMAIL
+```
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
 				import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
 				{
 					from_name: form.name,
-					to_name: "Tharunraj Mailvakanan",
+					to_name: VITE_APP_EMAILJS_TO_NAME,
 					from_email: form.email,
-					to_email: "tharunoptimus@proton.me",
+					to_email: VITE_APP_EMAILJS_TO_EMAIL,
 					message: form.message,
 				},
 				import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
