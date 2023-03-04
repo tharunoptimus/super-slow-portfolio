2f172481e69378bbb4795cf3ee436ad713d2153d
tharunoptimus <tharunoptimus@outlook.com>
2023-03-04 15:18:26 +0530

    Added loader component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
-import { motion } from "framer-motion";
+import { motion } from "framer-motion"
 
-import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
+import { styles } from "../styles"
+import { ComputersCanvas } from "./canvas"
 
 const Hero = () => {
 	return (
@@ -28,6 +28,24 @@ const Hero = () => {
 			</div>
 
 			<ComputersCanvas />
+
+			<div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+				<a href="#about">
+					<div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+						<motion.div
+							animate={{
+								y: [0, 24, 0],
+							}}
+							transition={{
+								duration: 1.5,
+								repeat: Infinity,
+								repeatType: "loop",
+							}}
+							className="w-3 h-3 rounded-full bg-secondary mb-1"
+						/>
+					</div>
+				</a>
+			</div>
 		</section>
 	)
 }
